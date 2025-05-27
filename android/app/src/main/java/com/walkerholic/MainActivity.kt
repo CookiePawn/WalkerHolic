af0ea7e142303e364a3c9efb1b1b314bb81722f2
CookiePawn <cookiepawn@naver.com>
2025-05-27 22:32:04 +0900

    [fix][package][fix package]

--- android/app/src/main/java/com/walkerholic/MainActivity.kt
+++ android/app/src/main/java/com/walkerholic/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.templatecli
+package com.walkerholic
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/walkerholic/MainApplication.kt
+++ android/app/src/main/java/com/walkerholic/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.templatecli
+package com.walkerholic
 
 import android.app.Application
 import com.facebook.react.PackageList
